534664d83baa9fe4e299bbcc40815606c79a6f40
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-09-10 23:53:00 -0300

    Add socket example

---
+++ src/socket.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char *argv[])
+{
+    int s;
+    int status;
+    struct addrinfo hints, *res, *p;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo("www.example.com", "http", &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
+    }
+
+    for (p = res; p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+
+        // trying openning socket
+        s = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        printf("  sock: %d\n", s);
+    }
+
+    freeaddrinfo(res);
+
+    exit(EXIT_SUCCESS);
+}
